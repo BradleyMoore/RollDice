5b682e25df06d3969e1afaa1a7b322be82fcdfdc
BradleyMoore <BradleyMooreTX@gmail.com>
2014-10-30 16:22:39 +0000

    create roll.py skeleton

---
+++ roll.py
@@ -0,0 +1,24 @@
+from random import randint
+from sys import argv
+
+
+def get_cl_args():
+    pass
+
+
+def roll(die):
+    pass
+
+
+
+def restart(state_list):
+    pass
+
+
+def main(num_of_die):
+    pass
+
+
+while __name__ == '__main__':
+    pass
+
